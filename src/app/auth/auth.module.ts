94f58c1baf9ff58caab3948ef9fea120b935b94f
Jefferson Ferreira <Jefferson1489@gmail.com>
2025-01-26 01:49:59 -0300

    ng update @angular/cli@18

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {LoginComponent} from "./login/login.component";
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    HttpClientModule,
-    LoginComponent
-  ]
-})
+@NgModule({ declarations: [], imports: [CommonModule,
+        AuthRoutingModule,
+        LoginComponent], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AuthModule { }
--- src/app/core/shared/components/icon/icon.component.ts
+++ src/app/core/shared/components/icon/icon.component.ts
@@ -1,18 +1,18 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {DomSanitizer} from "@angular/platform-browser";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-icon',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [],
   providers: [HttpClient],
   templateUrl: './icon.component.html',
   styleUrl: './icon.component.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class IconComponent implements OnInit {
-  @Input({required: true}) icon!: string;
+  @Input({ required: true }) icon!: string;
   @Input() cssClass!: string;
   iconeSVG: any;
 
@@ -23,7 +23,7 @@ export class IconComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.httpClient.get(`/assets/icons/${this.icon}.svg`, {responseType: "text"}).subscribe({
+    this.httpClient.get(`/assets/icons/${this.icon}.svg`, { responseType: "text" }).subscribe({
       next: (element) => {
         this.iconeSVG = this.domSanitizer.bypassSecurityTrustHtml(element);
       },
